a1b2b1243c01b80ac3c7ddb3c13bfbb4009b8a97
Puneet kumar <73667955+puneet2121@users.noreply.github.com>
2023-01-24 06:22:29 +0000

    write a simple program adding two numbers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  cout << "Hello World!\n";
+  int number1 = 0;
+  int number2 = 0;
+  std::cout << "Enter the first number: ";
+  std::cin >> number1;
+  std::cout << "Enter the second number: ";
+  std::cin >> number2;
+  
+  int sum = number1 + number2;
+  std::cout << "Sum is " << sum << std::endl;
 }
